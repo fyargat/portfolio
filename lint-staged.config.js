e7d20cd5573d1ca83911bc2d32d794329db12c4d
Cass <bikbaevf18@gmail.com>
2023-10-08 13:40:32 +0400

    Add lint-staged & husky

---
+++ lint-staged.config.js
@@ -0,0 +1,16 @@
+/* eslint-env node */
+const path = require('path');
+
+const eslintCommand = (filenames) =>
+  `next lint --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
+
+const formatCommand = 'prettier --write';
+
+const stylelintCommand = 'stylelint --allow-empty-input "**/*.{css,scss}"';
+
+module.exports = {
+  '*.{js,jsx,ts,tsx}': [formatCommand, eslintCommand],
+  '*.{css,scss}': [formatCommand, stylelintCommand],
+};
