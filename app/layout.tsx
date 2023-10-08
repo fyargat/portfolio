699059c9fd3cc92fa2e3f3958e08bde52d215181
Cass <bikbaevf18@gmail.com>
2023-10-08 13:14:55 +0400

    Add linters & prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
-import type { Metadata } from "next";
-import "../styles/index.scss";
+import type { Metadata } from 'next';
+
+import '@/styles/index.scss';
 
 export const metadata: Metadata = {
-  title: "Portfolio",
-  description: "Portfolio",
+  title: 'Portfolio',
+  description: 'Portfolio',
 };
 
 export default function RootLayout({
@@ -12,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>{children}</body>
     </html>
   );
