8c50fa4a8fe3db0f6795291399af2ace6caf9300
Cass <bikbaevf18@gmail.com>
2023-10-11 15:08:38 +0400

    Add constants

---
+++ constants/header.ts
@@ -0,0 +1,22 @@
+export const links = [
+  {
+    id: 1,
+    label: 'Projects',
+    delay: 0,
+  },
+  {
+    id: 2,
+    label: 'About',
+    delay: 0.1,
+  },
+  {
+    id: 3,
+    label: 'Skills',
+    delay: 0.2,
+  },
+  {
+    id: 4,
+    label: 'Contact',
+    delay: 0,
+  },
+];
