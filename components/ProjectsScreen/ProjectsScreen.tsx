15c5b91a9706c8e345c4fa8cddd2a6349b7c07f5
Cass <bikbaevf18@gmail.com>
2023-10-11 15:15:59 +0400

    Create & connect ProjectsScreen component

---
+++ components/ProjectsScreen/ProjectsScreen.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import styles from './ProjectsScreen.module.scss';
+
+interface IProps {}
+
+export const ProjectsScreen: FC<IProps> = () => {
+  return <div className={styles.container}>ProjectsScreen</div>;
+};
