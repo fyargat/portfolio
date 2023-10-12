57063fcf971f327f7ee9dfef4cc9c01479cf404d
Cass <bikbaevf18@gmail.com>
2023-10-13 03:14:20 +0400

    Add skills data

---
+++ constants/skills.ts
@@ -0,0 +1,188 @@
+import { Skills } from '@/types';
+
+export const FRONTEND_SKILLS: Skills = [
+  [
+    {
+      id: 1,
+      icon: 'html',
+      title: 'HTML',
+    },
+    {
+      id: 2,
+      icon: 'css',
+      title: 'CSS',
+    },
+    {
+      id: 3,
+      icon: 'javascript',
+      title: 'JavaScript',
+    },
+  ],
+  [
+    {
+      id: 4,
+      icon: 'react',
+      title: 'React',
+    },
+    {
+      id: 5,
+      icon: 'redux',
+      title: 'Redux',
+    },
+  ],
+  [
+    {
+      id: 6,
+      icon: 'vue',
+      title: 'Vue',
+    },
+    {
+      id: 7,
+      icon: 'nextjs',
+      title: 'NextJS',
+    },
+    {
+      id: 8,
+      icon: 'nuxt',
+      title: 'Nuxt',
+    },
+  ],
+  [
+    {
+      id: 9,
+      icon: 'sass',
+      title: 'Sass',
+    },
+    {
+      id: 10,
+      icon: 'axios',
+      title: 'Axios',
+    },
+  ],
+];
+
+export const BACKEND_SKILLS: Skills = [
+  [
+    {
+      id: 1,
+      icon: 'nodejs',
+      title: 'NodeJS',
+    },
+    {
+      id: 2,
+      icon: 'express',
+      title: 'Express',
+    },
+    {
+      id: 3,
+      icon: 'typescript',
+      title: 'TypeScript',
+    },
+  ],
+  [
+    {
+      id: 4,
+      icon: 'nestjs',
+      title: 'NestJS',
+    },
+    {
+      id: 5,
+      icon: 'socket-io',
+      title: 'Socket.io',
+    },
+  ],
+  [
+    {
+      id: 6,
+      icon: 'mongo',
+      title: 'MongoDB',
+    },
+
+    {
+      id: 7,
+      icon: 'mongoose',
+      title: 'Mongoose',
+    },
+
+    {
+      id: 8,
+      icon: 'redis',
+      title: 'Redis',
+    },
+  ],
+  [
+    {
+      id: 9,
+      icon: 'jest',
+      title: 'Jest',
+    },
+    {
+      id: 10,
+      icon: 'prisma',
+      title: 'Prisma',
+    },
+  ],
+];
+
+export const MISCELLANEOUS_SKILLS: Skills = [
+  [
+    {
+      id: 1,
+      icon: 'github-white',
+      title: 'GitHub',
+    },
+    {
+      id: 2,
+      icon: 'gitlab',
+      title: 'GitLab',
+    },
+    {
+      id: 3,
+      icon: 'docker',
+      title: 'Docker',
+    },
+  ],
+  [
+    {
+      id: 4,
+      icon: 'linux',
+      title: 'Linux',
+    },
+    {
+      id: 5,
+      icon: 'figma',
+      title: 'Figma',
+    },
+  ],
+  [
+    {
+      id: 6,
+      icon: 'sentry',
+      title: 'Sentry',
+    },
+
+    {
+      id: 7,
+      icon: 'webpack',
+      title: 'Webpack',
+    },
+
+    {
+      id: 8,
+      icon: 'pm2',
+      title: 'PM2',
+    },
+  ],
+  [
+    {
+      id: 9,
+      icon: 'github-actions',
+      title: 'GitHub Actions',
+    },
+    {
+      id: 10,
+      icon: 'eslint',
+      title: 'Eslint',
+    },
+  ],
+];
