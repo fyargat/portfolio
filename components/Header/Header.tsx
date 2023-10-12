e9ec3486bc3a02fdf509157c5585cf9438b04381
Cass <bikbaevf18@gmail.com>
2023-10-12 23:01:09 +0400

    Update scrolling

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -6,11 +6,9 @@ import { FC, useState } from 'react';
 
 import { Logo } from '@/components/Logo';
 
-import { Screen, TABLET_BREAKPOINT } from '@/constants';
-import { links } from '@/constants/header';
-import { MIN_SCROLL_Y_POSITION, ScrollYDirection } from '@/constants/scroll';
-import { useScreenWidth } from '@/hooks/useScreenWidth';
-import { useScrollY } from '@/hooks/useScrollY';
+import { Screen } from '@/constants';
+import { VisibilityStatus, links } from '@/constants/header';
+import { useHeaderScroll } from '@/hooks/useHeaderScroll';
 
 import styles from './Header.module.scss';
 
@@ -21,93 +19,90 @@ export const Header: FC<IProps> = () => {
 
   const toggle = () => setIsOpen((prev) => !prev);
 
-  const scrollY = useScrollY();
-  const screenWidth = useScreenWidth();
-
-  const isHeaderDown =
-    screenWidth >= TABLET_BREAKPOINT &&
-    scrollY.direction === ScrollYDirection.Top &&
-    scrollY.position !== MIN_SCROLL_Y_POSITION;
-
-  const handleScroll = (id: Screen) => () => {
-    const element = document.getElementById(id) as HTMLDivElement;
-
-    element.scrollIntoView({
-      behavior: 'smooth',
-    });
-  };
+  const { visibilityStatus, onScroll } = useHeaderScroll();
 
   return (
-    <header
-      className={cn(styles.header, {
-        [styles.headerOpen]: isOpen,
-        [styles.headerDown]: isHeaderDown,
-        [styles.headerUp]: false,
-      })}
-    >
-      <div className={styles.container}>
-        <div className={cn('wrap', styles.wrap)}>
-          <div className={cn(styles.logo, styles.logoDesktop)}>
-            <Logo />
-          </div>
+    <>
+      <header
+        className={cn(styles.header, {
+          [styles.headerOpen]: isOpen,
+          [styles.headerDown]: visibilityStatus === VisibilityStatus.Visible,
+          [styles.headerUp]: false,
+        })}
+      >
+        <div className={styles.container}>
+          <div className={cn('wrap', styles.wrap)}>
+            <div className={cn(styles.logo, styles.logoDesktop)}>
+              <button onClick={onScroll(Screen.Intro)}>
+                <Logo />
+              </button>
+            </div>
 
-          <nav className={cn(styles.nav, styles.navDesktop)}>
-            <ul className={styles.navList}>
-              {links.map((v) => (
-                <li className={styles.navItem} key={v.id}>
-                  <button
-                    onClick={handleScroll(v.value)}
-                    className={styles.navInteractiveElem}
-                  >
-                    {v.label}
-                  </button>
-                </li>
-              ))}
-            </ul>
-          </nav>
-
-          {isOpen ? (
-            <nav className={cn(styles.nav, styles.navMobile)}>
+            <nav className={cn(styles.nav, styles.navDesktop)}>
               <ul className={styles.navList}>
                 {links.map((v) => (
-                  <motion.li
-                    variants={{
-                      initial: { y: 20, opacity: 0 },
-                      visible: { y: 0, opacity: 1 },
-                    }}
-                    initial='initial'
-                    animate='visible'
-                    transition={{
-                      duration: 0.2,
-                      ease: 'easeInOut',
-                      delay: v.delay,
-                    }}
-                    className={styles.navItem}
-                    key={v.id}
-                  >
+                  <li className={styles.navItem} key={v.id}>
                     <button
-                      onClick={handleScroll(v.value)}
+                      onClick={onScroll(v.value)}
                       className={styles.navInteractiveElem}
                     >
                       {v.label}
                     </button>
-                  </motion.li>
+                  </li>
                 ))}
               </ul>
             </nav>
-          ) : (
-            <div className={cn(styles.logo, styles.logoMobile)}>
-              <Logo />
-            </div>
-          )}
 
-          <button onClick={toggle} className={styles.menu}>
-            <div className={styles.menuLine} />
-            <div className={styles.menuLine} />
-            <div className={styles.menuLine} />
-          </button>
+            {isOpen ? (
+              <nav className={cn(styles.nav, styles.navMobile)}>
+                <ul className={styles.navList}>
+                  {links.map((v) => (
+                    <motion.li
+                      variants={{
+                        initial: { y: 20, opacity: 0 },
+                        visible: { y: 0, opacity: 1 },
+                      }}
+                      initial='initial'
+                      animate='visible'
+                      transition={{
+                        duration: 0.2,
+                        ease: 'easeInOut',
+                        delay: v.delay,
+                      }}
+                      className={styles.navItem}
+                      key={v.id}
+                    >
+                      <button
+                        onClick={onScroll(v.value)}
+                        className={styles.navInteractiveElem}
+                      >
+                        {v.label}
+                      </button>
+                    </motion.li>
+                  ))}
+                </ul>
+              </nav>
+            ) : (
+              <div className={cn(styles.logo, styles.logoMobile)}>
+                <button onClick={onScroll(Screen.Intro)}>
+                  <Logo />
+                </button>
+              </div>
+            )}
+
+            <button onClick={toggle} className={styles.menu}>
+              <div className={styles.menuLine} />
+              <div className={styles.menuLine} />
+              <div className={styles.menuLine} />
+            </button>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+      <div
+        className={cn(styles.shadow, {
+          [styles.shadowVisible]: visibilityStatus === VisibilityStatus.Visible,
+        })}
+      />
+    </>
   );
 };
