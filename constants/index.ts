75785df2b476d9bd0453facbdd11fb5377c6616b
Cass <bikbaevf18@gmail.com>
2023-10-12 23:00:32 +0400

    Add id for IntroScreen

--- constants/index.ts
+++ constants/index.ts
@@ -2,6 +2,7 @@ export const TABLET_BREAKPOINT = 768;
 export const MOBILE_BREAKPOINT = 500;
 
 export enum Screen {
+  Intro = 'intro',
   Projects = 'projects',
   About = 'about',
   Skills = 'skills',
