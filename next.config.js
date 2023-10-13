a291f14b191ed0f370f3b70ba7a1fa55ec08354f
Cass <bikbaevf18@gmail.com>
2023-10-13 23:38:44 +0400

    Update next-config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const path = require('path');
+
+const nextConfig = {
+  output: 'standalone',
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './'),
+    };
+
+    return config;
+  },
+};
 
 module.exports = nextConfig;
